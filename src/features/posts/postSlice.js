244e93daeab3ea7e8dc3ce606ff89bf97525f698
Nilesh Kumar <131458541+NILESHSINGH234@users.noreply.github.com>
2024-05-10 04:04:59 +0530

    added services & slice reducers

--- src/features/posts/postSlice.js
+++ src/features/posts/postSlice.js
@@ -6,10 +6,21 @@ import { deletePostService } from "../../services/posts/postsServices";
 import { editPostService } from "../../services/posts/postsServices";
 import { getAllPostsService } from "../../services/posts/postsServices";
 import { getSinglePostService } from "../../services/posts/postsServices";
+import { likePostService } from "../../services/posts/postsServices";
+import { dislikePostService } from "../../services/posts/postsServices";
+import { getAllCommentsService } from "../../services/posts/postsServices";
+import { addCommentService } from "../../services/posts/postsServices";
+import { deleteCommentService } from "../../services/posts/postsServices";
+import { upvoteCommentService } from "../../services/posts/postsServices";
+import { downvoteCommentService } from "../../services/posts/postsServices";
+import { bookmarkPostService } from "../../services/posts/postsServices";
+import { deleteBookmarkPostService } from "../../services/posts/postsServices";
+import { getAllBookmarkPostService } from "../../services/posts/postsServices";
 const initialState = {
   allPosts: [],
   singlePost: {},
   userPosts: [],
+  bookmarkPosts: [],
   postStatus: "idle",
   postError: null,
   isPostModalOpen: false,
@@ -76,6 +87,114 @@ export const deletePost = createAsyncThunk(
   }
 );
 
+export const likePost = createAsyncThunk(
+    "posts/likePost",
+    async ({ postId, token }, { rejectWithValue }) => {
+      try {
+        const response = await likePostService(postId, token);
+        return response.data.posts;
+      } catch (error) {
+        return rejectWithValue(error.response.data);
+      }
+    }
+  );
+  
+  export const dislikePost = createAsyncThunk(
+    "posts/dislikePost",
+    async ({ postId, token }, { rejectWithValue }) => {
+      try {
+        const response = await dislikePostService(postId, token);
+        return response.data.posts;
+      } catch (error) {
+        return rejectWithValue(error.response.data);
+      }
+    }
+  );
+  
+  export const bookmarkPost = createAsyncThunk(
+    "posts/bookmarkPost",
+    async ({ postId, token }, { rejectWithValue }) => {
+      try {
+        const response = await bookmarkPostService(postId, token);
+        return response.data;
+      } catch (error) {
+        return rejectWithValue(error.response.data);
+      }
+    }
+  );
+  
+  export const removeBookmarkPost = createAsyncThunk(
+    "posts/removeBookmarkPost",
+    async ({ postId, token }, { rejectWithValue }) => {
+      try {
+        const response = await deleteBookmarkPostService(postId, token);
+        return response.data;
+      } catch (error) {
+        return rejectWithValue(error.response.data);
+      }
+    }
+  );
+  
+  export const getAllBookmarkPosts = createAsyncThunk(
+    "posts/getAllBookmarkPosts",
+    async (token, { rejectWithValue }) => {
+      try {
+        const response = await getAllBookmarkPostService(token);
+        return response.data;
+      } catch (error) {
+        return rejectWithValue(error.response.data);
+      }
+    }
+  );
+  
+  export const addComment = createAsyncThunk(
+    "posts/addComment",
+    async ({ postId, commentData, token }, { rejectWithValue }) => {
+      try {
+        const response = await addCommentService(postId, commentData, token);
+        return response.data.posts;
+      } catch (error) {
+        return rejectWithValue(error.response.data);
+      }
+    }
+  );
+  
+  export const deleteComment = createAsyncThunk(
+    "posts/deleteComment",
+    async ({ postId, commentId, token }, { rejectWithValue }) => {
+      try {
+        const response = await deleteCommentService(postId, commentId, token);
+        return response.data.posts;
+      } catch (error) {
+        return rejectWithValue(error.response.data);
+      }
+    }
+  );
+  
+  export const upvoteComment = createAsyncThunk(
+    "posts/upvoteComment",
+    async ({ postId, commentId, token }, { rejectWithValue }) => {
+      try {
+        const response = await upvoteCommentService(postId, commentId, token);
+        return response.data.posts;
+      } catch (error) {
+        return rejectWithValue(error.response.data);
+      }
+    }
+  );
+  
+  export const downvoteComment = createAsyncThunk(
+    "posts/downvoteComment",
+    async ({ postId, commentId, token }, { rejectWithValue }) => {
+      try {
+        const response = await downvoteCommentService(postId, commentId, token);
+        return response.data.posts;
+      } catch (error) {
+        return rejectWithValue(error.response.data);
+      }
+    }
+  );
+  
 const postSlice = createSlice({
     name: "posts",
     initialState,
@@ -128,6 +247,76 @@ const postSlice = createSlice({
           state.postError = payload;
         });
     },
+    [likePost.fulfilled]: (state, { payload }) => {
+        state.allPosts = payload;
+        toast.success("You liked the post");
+      },
+      [likePost.rejected]: (state, { payload }) => {
+        state.postError = payload;
+        toast.error("Some error occured. Try Again.");
+      },
+      [dislikePost.fulfilled]: (state, { payload }) => {
+        state.allPosts = payload;
+        toast.success("You disliked the post");
+      },
+      [dislikePost.rejected]: (state, { payload }) => {
+        state.postError = payload;
+        toast.error("Some error occured. Try Again.");
+      },
+      [bookmarkPost.fulfilled]: (state, { payload }) => {
+        state.bookmarkPosts = payload.bookmarks;
+        toast.success("Post bookmarked successfully.");
+      },
+      [bookmarkPost.rejected]: (state, { payload }) => {
+        state.postError = payload;
+        toast.error("Some error occured. Try Again.");
+      },
+      [removeBookmarkPost.fulfilled]: (state, { payload }) => {
+        state.bookmarkPosts = payload.bookmarks;
+        toast.success("Post removed from bookmark.");
+      },
+      [removeBookmarkPost.rejected]: (state, { payload }) => {
+        state.postError = payload;
+        toast.error("Some error occured. Try Again.");
+      },
+      [getAllBookmarkPosts.fulfilled]: (state, { payload }) => {
+        state.bookmarkPosts = payload.bookmarks;
+      },
+      [getAllBookmarkPosts.rejected]: (state, { payload }) => {
+        state.postError = payload;
+      },
+      [addComment.fulfilled]: (state, { payload }) => {
+        state.allPosts = payload;
+        toast.success("You commented!");
+      },
+      [addComment.rejected]: (state, { payload }) => {
+        state.postError = payload.errors;
+        toast.error("Some error occured. Try Again.");
+      },
+      [deleteComment.fulfilled]: (state, { payload }) => {
+        state.allPosts = payload;
+        toast.success("comment deleted!");
+      },
+      [deleteComment.rejected]: (state, { payload }) => {
+        state.postError = payload.errors;
+        toast.error("Some error occured. Try Again.");
+      },
+      [upvoteComment.fulfilled]: (state, { payload }) => {
+        state.allPosts = payload;
+        toast.success("You upvoted!");
+      },
+      [upvoteComment.rejected]: (state, { payload }) => {
+        state.postError = payload.errors;
+        toast.error("Some error occured. Try Again.");
+      },
+      [downvoteComment.fulfilled]: (state, { payload }) => {
+        state.allPosts = payload;
+        toast.success("You downvoted!");
+      },
+      [downvoteComment.rejected]: (state, { payload }) => {
+        state.postError = payload.errors;
+        toast.error("Some error occured. Try Again.");
+      },
   });
   
 export const { setPostModalOpen } = postSlice.actions;
--- src/services/posts/postsServices.js
+++ src/services/posts/postsServices.js
@@ -41,3 +41,112 @@ export const editPostService = (postData, token) =>
       },
     }
   );
+
+  export const likePostService = (postId, token) =>
+    axios.post(
+      `/api/posts/like/${postId}`,
+      {},
+      {
+        headers: {
+          authorization: token,
+        },
+      }
+    );
+  
+  // DISLIKE A POST
+  export const dislikePostService = (postId, token) =>
+    axios.post(
+      `/api/posts/dislike/${postId}`,
+      {},
+      {
+        headers: {
+          authorization: token,
+        },
+      }
+    );
+  
+  // GET COMMENTS
+  export const getAllCommentsService = postId =>
+    axios.get(`/api/comments/${postId}`);
+  
+  // ADD COMMENT
+  export const addCommentService = (postId, commentData, token) =>
+    axios.post(
+      `/api/comments/add/${postId}`,
+      {
+        commentData,
+      },
+      {
+        headers: {
+          authorization: token,
+        },
+      }
+    );
+  
+  // DELETE COMMENT
+  export const deleteCommentService = (postId, commentId, token) =>
+    axios.post(
+      `/api/comments/delete/${postId}/${commentId}`,
+      {},
+      {
+        headers: {
+          authorization: token,
+        },
+      }
+    );
+  
+  // UPVOTE A COMMENT
+  export const upvoteCommentService = (postId, commentId, token) =>
+    axios.post(
+      `/api/comments/upvote/${postId}/${commentId}`,
+      {},
+      {
+        headers: {
+          authorization: token,
+        },
+      }
+    );
+  
+  // DOWNVOTE A COMMENT
+  export const downvoteCommentService = (postId, commentId, token) =>
+    axios.post(
+      `/api/comments/downvote/${postId}/${commentId}`,
+      {},
+      {
+        headers: {
+          authorization: token,
+        },
+      }
+    );
+  
+  // ADD POST TO USER BOOKMARKS
+  export const bookmarkPostService = (postId, token) =>
+    axios.post(
+      `/api/users/bookmark/${postId}`,
+      {},
+      {
+        headers: {
+          authorization: token,
+        },
+      }
+    );
+  
+  // DELETE POST FROM USER BOOKMARKS
+  export const deleteBookmarkPostService = (postId, token) =>
+    axios.post(
+      `/api/users/remove-bookmark/${postId}`,
+      {},
+      {
+        headers: {
+          authorization: token,
+        },
+      }
+    );
+  
+  // GET ALL BOOKMARKED POSTS
+  export const getAllBookmarkPostService = token =>
+    axios.get("/api/users/bookmark/", {
+      headers: {
+        authorization: token,
+      },
+    });
