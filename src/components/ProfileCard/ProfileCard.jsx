c3d6c47593d327e513ad636f31d0d1aa80145b1b
Nilesh Kumar <131458541+NILESHSINGH234@users.noreply.github.com>
2024-05-10 09:57:32 +0530

    feat: added profile page

--- src/components/ProfileCard/ProfileCard.jsx
+++ src/components/ProfileCard/ProfileCard.jsx
@@ -1,51 +1,118 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { LinkIcon } from "@heroicons/react/outline";
+import { useSelector, useDispatch } from "react-redux";
+import { EditProfileModal } from "../Modals/EditProfileModal";
+import { followUser } from "../../features/users/userSlice";
+import { unfollowUser } from "../../features/users/userSlice";
+import { getAllPosts } from "../../features/posts/postSlice";
+export const ProfileCard = ({ userDetails }) => {
+  const [isEditModalOpen, setIsEditModalOpen] = useState(false);
+  const {
+    _id,
+    firstName,
+    lastName,
+    avatar,
+    username,
+    bio,
+    portfolio,
+    followers,
+    following,
+  } = userDetails;
 
-export const ProfileCard = () => {
+  const { userInfo, token } = useSelector(state => state.auth);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(getAllPosts());
+  }, [dispatch]);
+    
+
+  const isUserAlreadyFollowing = () =>
+    followers?.find(user => user.username === userInfo.username);
+
   return (
-    <div className="pb-3 border-b border-gray-700">
-      <div className="h-36 sm:h-48 bg-[#1a85cd]"></div>
-      <div className="p-4">
-        <div className="flex justify-between items-start pb-4">
-          <img
-            src="https://i.pravatar.cc/300?img=11"
-            alt="avatar"
-            className="h-[120px] w-[120px] sm:h-36 sm:w-36 rounded-full border-4 border-[#151F2B] -mt-20"
-          />
-          <button className="ml-auto bg-[#151F2B] hover:bg-[#273340] text-white border border-gray-500 rounded-full text-base font-bold py-1.5 px-3.5">
-            Edit Profile
-          </button>
-        </div>
-        <div className="flex flex-col space-y-3">
-          <div className="">
-            <h2 className="text-xl font-extrabold text-[#F7F9F9] leading-tight">
-              John Sharma
-            </h2>
-            <span className="text-gray-500 text-[15px]">@johnSharma</span>
+    <>
+      <div className="pb-3 border-b border-gray-700">
+        <div className="h-36 sm:h-40 bg-[#1a85cd]"></div>
+        <div className="p-4">
+          <div className="flex justify-between items-start pb-4">
+            {avatar ? (
+              <img
+                src={avatar}
+                alt="avatar"
+                className="h-[120px] w-[120px] bg-[#273340] sm:h-36 sm:w-36 rounded-full border-4 border-[#151F2B] -mt-20"
+              />
+            ) : (
+              <div className="h-[120px] w-[120px] bg-gray-700 sm:h-36 sm:w-36 rounded-full border-4 border-[#151F2B] -mt-20">
+                <h4 className="w-full h-full flex items-center justify-center text-3xl text-white font-bold">
+                  AK
+                </h4>
+              </div>
+            )}
+            {userInfo.username === username ? (
+              <button
+                className="ml-auto bg-[#151F2B] hover:bg-[#273340] text-white border border-gray-500 rounded-full text-base font-bold py-1.5 px-3.5"
+                onClick={() => setIsEditModalOpen(true)}
+              >
+                Edit Profile
+              </button>
+            ) : isUserAlreadyFollowing() ? (
+              <button
+                className="ml-auto bg-[#151F2B] hover:bg-[#273340] text-white border border-gray-500 rounded-full text-base font-bold py-1.5 px-3.5"
+                onClick={() =>
+                  dispatch(unfollowUser({ followUserId: _id, token }))
+                }
+              >
+                Unfollow
+              </button>
+            ) : (
+              <button
+                className="ml-auto bg-[#EFF3F4] hover:bg-[#e0e0e0] text-black border border-gray-500 rounded-full text-base font-bold py-1.5 px-3.5"
+                onClick={() =>
+                  dispatch(followUser({ followUserId: _id, token }))
+                }
+              >
+                Follow
+              </button>
+            )}
           </div>
-
-          <h4 className="text-[#F7F9F9] text-[15px]">
-            Front-end Developer | JavaScript 💛
-          </h4>
-          <div className="flex items-center space-x-1">
-            <LinkIcon className="h-5 text-gray-500" />
-            <a
-              href="https://github.com/AbhishekkGautam"
-              className="text-[#1A8CD8] hover:underline"
-            >
-              github.com/AbhishekkGautam
-            </a>
-          </div>
-          <div className="flex items-center space-x-5">
-            <div className="text-[#F7F9F9] font-bold text-[15px] hover:underline cursor-pointer">
-              193 <span className="text-gray-500 font-medium">Following</span>
+          <div className="flex flex-col space-y-3">
+            <div className="">
+              <h2 className="text-xl font-extrabold text-[#F7F9F9] leading-tight">
+                {firstName} {lastName}
+              </h2>
+              <span className="text-gray-500 text-[15px]">@{username}</span>
             </div>
-            <div className="text-[#F7F9F9] font-bold text-[15px] hover:underline cursor-pointer">
-              463 <span className="text-gray-500 font-medium">Followers</span>
+            <h4 className="text-[#F7F9F9] text-[15px]">{bio}</h4>
+            {portfolio && (
+              <div className="flex items-center space-x-1">
+                <LinkIcon className="h-5 text-gray-500" />
+                <a href={portfolio} className="text-[#1A8CD8] hover:underline">
+                  {portfolio}
+                </a>
+              </div>
+            )}
+
+            <div className="flex items-center space-x-5">
+              <div className="text-[#F7F9F9] font-bold text-[15px] hover:underline cursor-pointer">
+                {following?.length}{" "}
+                <span className="text-gray-500 font-medium">Following</span>
+              </div>
+              <div className="text-[#F7F9F9] font-bold text-[15px] hover:underline cursor-pointer">
+                {followers?.length}{" "}
+                <span className="text-gray-500 font-medium">Followers</span>
+              </div>
             </div>
           </div>
         </div>
       </div>
-    </div>
+      {isEditModalOpen && (
+        <EditProfileModal
+          isEditModalOpen={isEditModalOpen}
+          setIsEditModalOpen={setIsEditModalOpen}
+          profileData={userDetails}
+        />
+      )}
+    </>
   );
 };
--- src/pages/Profile/Profile.jsx
+++ src/pages/Profile/Profile.jsx
@@ -1,22 +1,59 @@
-import { ArrowLeftIcon } from "@heroicons/react/outline";
-import React from "react";
+import React, { useEffect } from "react";
+import { ArrowLeftIcon } from "@heroicons/react/outline"
+import { useDispatch, useSelector } from "react-redux";
+import { useParams, useNavigate } from "react-router-dom";
 import { BottomNavigation } from "../../components/BottomNavigation/BottomNavigation";
 import { Sidebar } from "../../components/Sidebar/Sidebar";
 import { ProfileCard } from "../../components/ProfileCard/ProfileCard";
-
+import { Post } from "../../components/Post/Post";
 import { Widgets } from "../../components/Widgets/Widgets";
+import { getSingleUser } from "../../features/users/userSlice";
+import { getUserPostsByUsername } from "../../features/users/userSlice";
+import { resetUserProfile } from "../../features/users/userSlice";
 export const Profile = () => {
+  const { username } = useParams();
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    dispatch(getSingleUser({ username }));
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+    return () => {
+      dispatch(resetUserProfile());
+    };
+  }, [dispatch, username]);
+
+  const { singleUser, userPosts } = useSelector(state => state.users);
+  const { allPosts } = useSelector(state => state.posts);
+
+  useEffect(() => {
+    dispatch(getUserPostsByUsername({ username }));
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [allPosts]);
   return (
     <main className="min-h-screen bg-[#151F2B] flex max-w-[1500px] mx-auto">
       <Sidebar />
       <div className="flex-grow border-l border-r border-gray-700 max-w-[600px] sm:ml-[72px] xl:ml-[340px]">
         <div className="flex items-center px-1.5 py-2 border-b border-gray-700 text-[#d9d9d9] font-semibold text-xl gap-x-4 sticky top-0 z-50 bg-[rgba(21, 32, 43, 0.75)] backdrop-blur-md backdrop-brightness-100">
           <div className="hoverAnimation w-9 h-9 flex items-center justify-center xl:px-0">
-            <ArrowLeftIcon className="h-5 text-white" />
+          <ArrowLeftIcon
+              className="h-5 text-white"
+              onClick={() => navigate(-1)}
+            />
           </div>
-          Profile
+          {singleUser.firstName} {singleUser.lastName}
         </div>
-        <ProfileCard />
+        <ProfileCard userDetails={singleUser} />
+        <div className="pb-72">
+          {userPosts?.map((post, id) => {
+            return (
+              <div key={id}>
+                <Post postData={post} />
+              </div>
+            );
+          })}
+        </div>
+        
        
       </div>
       <Widgets />
